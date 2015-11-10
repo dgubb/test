b57f3412696314cf3c53b0fb5f4cf9c8f718d7d6
Gubb, David <david.gubb@ccfe.ac.uk>
2015-11-10 14:40:20 +0000

    refactored read file into function

--- searchShots.py
+++ searchShots.py
@@ -3,12 +3,18 @@
 import numpy
 import sys
 
+def process_file(file):
+   data = numpy.loadtxt(fname=file, delimiter=',')
+   if data[2,0] < 2:
+      print( file,data[2,0])
+
+
 input_len=len(sys.argv)
 if input_len !=2:
    print('incorrect number of inputs')
    sys.exit(1)
 
 file = sys.argv[1]
-data = numpy.loadtxt(fname=file, delimiter=',')
 
-print( data)
+process_file(file)
+
